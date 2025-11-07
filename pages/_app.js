ec2f0c913be82f4ac58f2ab4bd22239305ac9508
desktopvonzuben-rgb <desktopvonzuben@gmail.com>
2025-11-07 13:17:49 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BVHEWHZL5J"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BVHEWHZL5J');
+            `,
+          }}
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
